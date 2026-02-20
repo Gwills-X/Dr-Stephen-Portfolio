e500a06c3b98d6f905c6dc4690939a7563740cdc
Godswill Eguavoen <godswilleguavoen@gmail.com>
2026-02-20 20:04:10 +0100

    changed some things

--- src/components/research/publicationsPdf.js
+++ src/components/research/publicationsPdf.js
@@ -1,96 +1,94 @@
 export const publications = [
   {
     title:
-      "Iridium(III)-based minor groove binding complexes as DNAphotocleavage agents",
+      "Iridium(III)-based minor groove binding complexes as DNA photocleavage agents",
     journal: "Dalton Transactions",
-    file: "../../assets/publicationsPdf/Publication 1.pdf",
+    file: "/publications/Publication-1.pdf",
   },
   {
     title:
       "Nanoparticles for super resolution microscopy: intracellular delivery and molecular targeting",
     journal: "Chem Soc Rev",
-    file: "../../assets/publicationsPdf/Publication 2.pdf",
+    file: "/publications/Publication-2.pdf",
   },
   {
     title: "Nanocarriers used as probes for super-resolution microscopy",
     journal: "Materials Chemistry Frontiers",
-    file: "../../assets/publicationsPdf/Publication 3.pdf",
+    file: "/publications/publication-3.pdf",
   },
   {
     title:
       "Multi-nuclear silver complexes constructed from bis(benzimidazole)-2-oxapropane ligands and methacrylate",
     journal: "RSC Advances",
-    file: "../../assets/publicationsPdf/Publication 4.pdf",
+    file: "/publications/publication-4.pdf",
   },
   {
     title:
       "Synthesis, crystal structures, and DNA-binding studies of two silver(I) complexes with 1,3-bis(1-ethylbenzimidazol-2-yl)-2-thiapropane",
     journal: "Journal of Coordination Chemistry",
-    file: "../../assets/publicationsPdf/Publication 5.pdf",
+    file: "/publications/publication-5.pdf",
   },
   {
     title:
       "Quanterpyridine: synthesis, crystal-structure description, and Hirshfeld surface analysis",
     journal: "Crystallographic Communications",
-    file: "../../assets/publicationsPdf/Publication 6.pdf",
+    file: "/publications/publication-6.pdf",
   },
   {
     title:
       "Synthesis and studies of two proton-receptor fluorescent probes based on 1,8-naphthalimide",
     journal: "Coloration Technology",
-    file: "../../assets/publicationsPdf/Publication 7.pdf",
+    file: "/publications/publication-7.pdf",
   },
   {
     title:
-      "A new, highly potent 1,8-naphthalimide-based fluorescence turn-off chemosensor capable of detecting Cu(II) ions in real-world water samples",
+      "A new, highly potent 1,8-naphthalimide-based fluorescence turn-off chemosensor capable of detecting Cu(II) ions",
     journal: "Coloration Technology",
-    file: "../../assets/publicationsPdf/Publication 8.pdf",
+    file: "/publications/publication-8.pdf",
   },
   {
     title:
       "A three-coordinate Ag(I) complex based on the V-shaped ligand 1,3-bis(1-ethylbenzimidazol-2-yl)-2-thiapropane",
     journal: "Journal of Chemical Research",
-    file: "../../assets/publicationsPdf/Publication 9.pdf",
+    file: "/publications/publication-9.pdf",
   },
   {
     title:
-      "Fluorescent and colourimetric 1, 8-naphthalimide-appended chemosensors for the tracking of metal ions",
+      "Fluorescent and colourimetric 1,8-naphthalimide-appended chemosensors for tracking metal ions",
     journal: "CrossMark",
-    file: "../../assets/publicationsPdf/Publication 10.pdf",
+    file: "/publications/publication-10.pdf",
   },
   {
     title:
-      "Fluorescent, colourimetric, and ratiometric probes based on diverse fluorophore motifs for mercuric(II) ion (Hg2+) sensing ",
+      "Fluorescent, colourimetric, and ratiometric probes for mercuric(II) ion sensing",
     journal: "Check for updates",
-    file: "../../assets/publicationsPdf/Publication 11.pdf",
+    file: "/publications/publication-11.pdf",
   },
   {
-    title:
-      "A highly Selective Fluorescent Sensor for Monitoring Cu2+ Ion: Synthesis, Characterization and Photophysical Properties",
+    title: "Highly Selective Fluorescent Sensor for Monitoring Cu2+ Ion",
     journal: "CrossMark",
-    file: "../../assets/publicationsPdf/Publication 12.pdf",
+    file: "/publications/publication-12.pdf",
   },
   {
     title:
-      "A highly selective fluorescent chemosensor based on naphthalimide and Schiff base units for Cu2+ detection in aqueous medium",
+      "Highly selective fluorescent chemosensor based on naphthalimide and Schiff base units",
     journal: "De Gruyter",
-    file: "../../assets/publicationsPdf/Publication 13.pdf",
+    file: "/publications/publication-13.pdf",
+  },
+  {
+    title: "New highly effective fluorescent probe for Al3+ ions",
+    journal: "Journal of Photochemistry and Photobiology A: Chemistry",
+    file: "/publications/publication-14.pdf",
   },
   {
     title:
-      "A new highly effective fluorescent probe for AI3+ ions and its application in practical samples",
-    journal: "Journal Of Photochemistry and Photobiology A: Chemistry",
-    file: "../../assets/publicationsPdf/Publication 14.pdf",
-  },
-  {
-    title:
-      "DEVELOPMENT OF A NEW 4-AMINO-1,8-NAPHTHALIMIDE DERIVATIVE  AS A FLUORESCENT PROBE FOR MONITORING THE DIVALENT COPPER ION ",
-    journal: "JOURNAL OF APPLIED SPECTROSCOPY",
-    file: "../../assets/publicationsPdf/Publication 15.pdf",
+      "Development of a new 4-amino-1,8-naphthalimide derivative as a fluorescent probe",
+    journal: "Journal of Applied Spectroscopy",
+    file: "/publications/publication-15.pdf",
   },
   {
     title: "A diverse view of science to catalyse change",
     journal: "Check for updates",
-    file: "../../assets/publicationsPdf/Publication 16.pdf",
+    file: "/publications/publication-16.pdf",
   },
 ];
