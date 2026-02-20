c22dc4326f96059db0cd2c14585b45318d344418
Godswill Eguavoen <godswilleguavoen@gmail.com>
2026-02-20 20:08:12 +0100

    added all the pdfs

--- src/components/research/publicationsPdf.js
+++ src/components/research/publicationsPdf.js
@@ -14,81 +14,81 @@ export const publications = [
   {
     title: "Nanocarriers used as probes for super-resolution microscopy",
     journal: "Materials Chemistry Frontiers",
-    file: "/publications/publication-3.pdf",
+    file: "/publications/Publication-3.pdf",
   },
   {
     title:
       "Multi-nuclear silver complexes constructed from bis(benzimidazole)-2-oxapropane ligands and methacrylate",
     journal: "RSC Advances",
-    file: "/publications/publication-4.pdf",
+    file: "/publications/Publication-4.pdf",
   },
   {
     title:
       "Synthesis, crystal structures, and DNA-binding studies of two silver(I) complexes with 1,3-bis(1-ethylbenzimidazol-2-yl)-2-thiapropane",
     journal: "Journal of Coordination Chemistry",
-    file: "/publications/publication-5.pdf",
+    file: "/publications/Publication-5.pdf",
   },
   {
     title:
       "Quanterpyridine: synthesis, crystal-structure description, and Hirshfeld surface analysis",
     journal: "Crystallographic Communications",
-    file: "/publications/publication-6.pdf",
+    file: "/publications/Publication-6.pdf",
   },
   {
     title:
       "Synthesis and studies of two proton-receptor fluorescent probes based on 1,8-naphthalimide",
     journal: "Coloration Technology",
-    file: "/publications/publication-7.pdf",
+    file: "/publications/Publication-7.pdf",
   },
   {
     title:
       "A new, highly potent 1,8-naphthalimide-based fluorescence turn-off chemosensor capable of detecting Cu(II) ions",
     journal: "Coloration Technology",
-    file: "/publications/publication-8.pdf",
+    file: "/publications/Publication-8.pdf",
   },
   {
     title:
       "A three-coordinate Ag(I) complex based on the V-shaped ligand 1,3-bis(1-ethylbenzimidazol-2-yl)-2-thiapropane",
     journal: "Journal of Chemical Research",
-    file: "/publications/publication-9.pdf",
+    file: "/publications/Publication-9.pdf",
   },
   {
     title:
       "Fluorescent and colourimetric 1,8-naphthalimide-appended chemosensors for tracking metal ions",
     journal: "CrossMark",
-    file: "/publications/publication-10.pdf",
+    file: "/publications/Publication-10.pdf",
   },
   {
     title:
       "Fluorescent, colourimetric, and ratiometric probes for mercuric(II) ion sensing",
     journal: "Check for updates",
-    file: "/publications/publication-11.pdf",
+    file: "/publications/Publication-11.pdf",
   },
   {
     title: "Highly Selective Fluorescent Sensor for Monitoring Cu2+ Ion",
     journal: "CrossMark",
-    file: "/publications/publication-12.pdf",
+    file: "/publications/Publication-12.pdf",
   },
   {
     title:
       "Highly selective fluorescent chemosensor based on naphthalimide and Schiff base units",
     journal: "De Gruyter",
-    file: "/publications/publication-13.pdf",
+    file: "/publications/Publication-13.pdf",
   },
   {
     title: "New highly effective fluorescent probe for Al3+ ions",
     journal: "Journal of Photochemistry and Photobiology A: Chemistry",
-    file: "/publications/publication-14.pdf",
+    file: "/publications/Publication-14.pdf",
   },
   {
     title:
       "Development of a new 4-amino-1,8-naphthalimide derivative as a fluorescent probe",
     journal: "Journal of Applied Spectroscopy",
-    file: "/publications/publication-15.pdf",
+    file: "/publications/Publication-15.pdf",
   },
   {
     title: "A diverse view of science to catalyse change",
     journal: "Check for updates",
-    file: "/publications/publication-16.pdf",
+    file: "/publications/Publication-16.pdf",
   },
 ];
